6cdc3ed1eae131191e429e0cc4954232203fb2c4
Lars Tesmer <lars.tesmer@gmail.com>
2013-10-24 22:17:17 +0200

    Spec PluginFactory: Creates CurrentDay

--- spec/PluginFactorySpec.js
+++ spec/PluginFactorySpec.js
@@ -1,9 +1,10 @@
 describe("PluginFactory", function() {
 	var plugin_factory;
+	var emitter;
 
 	beforeEach(function() {
-		var emitter = jasmine.createSpy('Emitter');
 		var view_helper = jasmine.createSpy('ViewHelper');
+		emitter = jasmine.createSpy('Emitter');
 		plugin_factory = new PluginFactory(emitter, view_helper); 
 	});
 
@@ -13,6 +14,11 @@ describe("PluginFactory", function() {
 		}
 		var expected_error = new Error('Plugin does not exist with name: urglburgl');
 		expect(get_unknown_plugin).toThrow(expected_error);
+	});
+
+	it("creates a CurrentDay plugin", function() {
+		var actual = plugin_factory.getPlugin('current-day');
+		expect(actual).toEqual(new CurrentDay(emitter));
 	});
 });
 
