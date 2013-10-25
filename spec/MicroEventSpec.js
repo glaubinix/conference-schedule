503506d8661c8565fe373239a6acc320bd00c560
Lars Tesmer <lars.tesmer@gmail.com>
2013-10-25 22:20:30 +0200

    Spec MicroEvent: Bind several functions to one event

--- spec/MicroEventSpec.js
+++ spec/MicroEventSpec.js
@@ -19,5 +19,32 @@ describe("MicroEvent", function() {
 		expect(event_funcs.foo.calls.length).toEqual(1);
 	});
 
+	it("can bind a several functions to one event", function() {
+		var event_funcs = {
+			foo: function() {},
+			bar: function() {},
+			baz: function() {}
+		};
+
+		spyOn(event_funcs, 'foo');
+		spyOn(event_funcs, 'bar');
+		spyOn(event_funcs, 'baz');
+
+		micro_event.bind('event_one', event_funcs.foo);
+		micro_event.bind('event_one', event_funcs.bar);
+		micro_event.bind('event_one', event_funcs.baz);
+
+		micro_event.trigger('event_one');
+
+		expect(event_funcs.foo).toHaveBeenCalled();
+		expect(event_funcs.foo.calls.length).toEqual(1);
+
+		expect(event_funcs.bar).toHaveBeenCalled();
+		expect(event_funcs.bar.calls.length).toEqual(1);
+
+		expect(event_funcs.baz).toHaveBeenCalled();
+		expect(event_funcs.baz.calls.length).toEqual(1);
+	});
+
 });
 
