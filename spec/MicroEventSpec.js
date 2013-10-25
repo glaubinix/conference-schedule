4e5a6cb3989e57cb42b126926590e138db00ac3c
Lars Tesmer <lars.tesmer@gmail.com>
2013-10-25 22:04:23 +0200

    Spec MicroEvent: Binding single function to an event

--- spec/MicroEventSpec.js
+++ spec/MicroEventSpec.js
@@ -5,7 +5,18 @@ describe("MicroEvent", function() {
 		micro_event = new MicroEvent(); 
 	});
 
-	it("is a test", function() {
+	it("can bind a single function to an event", function() {
+		var event_funcs = {
+			foo: function() { return }
+		};
+
+		spyOn(event_funcs, 'foo');
+
+		micro_event.bind('event_one', event_funcs.foo);
+		micro_event.trigger('event_one');
+
+		expect(event_funcs.foo).toHaveBeenCalled();
+		expect(event_funcs.foo.calls.length).toEqual(1);
 	});
 
 });
