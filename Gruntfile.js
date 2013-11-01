78154480b0c027b2f880e2f2e8a51c5ebec28560
Stephan Vock <stephan.vock@innogames.de>
2013-11-02 00:48:58 +0100

    added css concat and cssmin to build prozess so far we only create the minified css file. I do not really know how to add it later J

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,28 @@
+var config = require('./config.json')
+
 module.exports = function (grunt) {
 	"use strict";
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		cssc: {
+			build: {
+				options: {
+					consolidateViaDeclarations: true,
+					consolidateViaSelectors:    true,
+					consolidateMediaQueries:    true
+				},
+				files: {
+					'build/css/master.css': ['css/normalize.css', 'css/main.css', 'css/themes/' + config['theme'] + '.css']
+				}
+			}
+		},
+		cssmin: {
+			build: {
+				src: 'build/css/master.css',
+				dest: 'build/css/master.css'
+			}
+		},
 		jasmine: {
 			src: [
 				"js/libs/microevent.js",
@@ -23,10 +43,11 @@ module.exports = function (grunt) {
 				specs: "spec/**/*.js"
 			}
 		}
-	})
+	});
 
-	grunt.loadNpmTasks('grunt-contrib-jasmine')
+	require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
 
+	grunt.registerTask('buildcss',  ['cssc', 'cssmin']);
 	grunt.registerTask('test', ['jasmine'])
 	grunt.registerTask('default', ['test'])
 };
