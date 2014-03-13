25149fad0f2833608e3286a6bb168e49a612e61b
Stephan Vock <stephan.vock@innogames.de>
2014-03-13 17:18:52 +0100

    to be honest the star feature is nice but with local storage integration it is just so much useful I currently use the generated talk id which does not react to changes in the schedule yet One way to solve this is to hash the talk object and identify the talk through hash

--- js/plugins/star.js
+++ js/plugins/star.js
@@ -5,20 +5,35 @@ function Star(emitter, view_helper) {
 
 Star.prototype.registerPlugin = function() {
 	var self = this;
+
+	var starHash = window.localStorage ? JSON.parse(localStorage.getItem("plugin-star")) : {};
+	if (!starHash) {
+		starHash = {};
+	}
+
 	this.emitter.bind('schedule-rendered', function() {
 		self.view_helper.applyForSelector('description', function(element) {
 			element.innerHTML += '<div><span class="star-button"></span></div>';
 		});
 
 		self.view_helper.applyForSelector('star-button', function(element) {
-			var talk_element = element.parentNode.parentNode.parentNode;
+			var talk_element = element.parentNode.parentNode.parentNode,
+				talkId = talk_element.dataset['talkId'];
+
+			if (starHash[talkId]) {
+				self.view_helper.addClass(talk_element, 'starred');
+			}
 
 			element.innerText = (self.view_helper.hasClass(talk_element, 'starred') ? 'unstar' : 'star');
 
 			element.addEventListener('click', function(event) {
+				var active = self.view_helper.hasClass(talk_element, 'starred');
+				starHash[talkId] = !active;
+				localStorage.setItem("plugin-star", JSON.stringify(starHash));
+
 				event.stopPropagation();
 				self.view_helper.toggleClass(talk_element, 'starred');
-				element.innerText = (self.view_helper.hasClass(talk_element, 'starred') ? 'unstar' : 'star');
+				element.innerText = (active ? 'unstar' : 'star');
 			});
 		});
 	});
