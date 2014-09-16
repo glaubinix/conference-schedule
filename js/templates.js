e48f131b70f354179d759607b881bcc662ab028b
Stephan Vock <stephan.vock@gmail.com>
2014-09-17 00:40:27 +0200

    added build process

--- js/templates.js
+++ js/templates.js
@@ -20,7 +20,7 @@
 
     // head.jade compiled template
     templatizer["head"] = function tmpl_head() {
-        return '<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0"/><meta name="apple-mobile-web-app-capable" content="yes"/><link rel="stylesheet" href="css/normalize.css"/><link rel="stylesheet" href="css/main.css"/>';
+        return '<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0"/><meta name="apple-mobile-web-app-capable" content="yes"/>';
     };
 
     // pages/schedule.jade compiled template
